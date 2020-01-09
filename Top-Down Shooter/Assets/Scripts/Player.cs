d1bff3cc624e70559eb8fc466470028c64ad771d
Samuel Eskilson <samuel.eskilson@gmail.com>
2020-01-10 00:13:09 +0100

    added player controller
    movement and rotation towards the where the mouse is pointing

---
+++ Top-Down Shooter/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class Player : MonoBehaviour
+{
+    Camera viewCamera;
+
+    public float moveSpeed = 7;
+
+    PlayerController controller;
+
+    void Start()
+    {
+        controller = GetComponent<PlayerController>();
+        viewCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = input.normalized * moveSpeed;
+        controller.Move(moveVelocity);
+
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if (groundPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            //Debug.DrawLine(ray.origin, point, Color.red);
+            controller.LookAt(point);
+        }
+    }
+}
