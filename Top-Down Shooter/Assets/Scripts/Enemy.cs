0ea426745e0c7866cdbd44c3d13d5d2ebef40def
Samuel Eskilson <samuel.eskilson@gmail.com>
2020-01-17 17:23:17 +0100

    added enemy attack
    enemies turn red and lunge at the player when within distance, no player damage yet

--- Top-Down Shooter/Assets/Scripts/Enemy.cs
+++ Top-Down Shooter/Assets/Scripts/Enemy.cs
@@ -7,16 +7,79 @@ using UnityEngine.AI;
 public class Enemy : LivingEntity
 {
 
+    public enum State { Idle, Attacking, Chasing };
+    State currentState;
+
     NavMeshAgent pathfinder;
     Transform target;
+    Material skinMaterial;
+
+    Color originalColor;
+
+    float attackDistanceThreshold = 0.5f;
+    float timeBetweenAttacks = 1;
+    float nextAttackTime;
+    float myCollisionRadius;
+    float targetCollisionRadius;
 
     protected override void Start()
     {
         base.Start();
         pathfinder = GetComponent<NavMeshAgent>();
-        target = GameObject.FindGameObjectWithTag("Player").transform;
-        StartCoroutine(UpdatePath());
+        skinMaterial = GetComponent<Renderer>().material;
+        originalColor = skinMaterial.color;
 
+        currentState = State.Chasing;
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        myCollisionRadius = GetComponent<CapsuleCollider>().radius;
+        targetCollisionRadius = target.GetComponent<CapsuleCollider>().radius;
+
+        StartCoroutine(UpdatePath());
+    }
+
+    public void Update()
+    {
+        if (Time.time > nextAttackTime)
+        {
+            float sqtDstToTarget = (target.position - transform.position).sqrMagnitude;
+            if (sqtDstToTarget < Mathf.Pow(attackDistanceThreshold + myCollisionRadius + targetCollisionRadius, 2))
+            {
+                nextAttackTime = Time.time + timeBetweenAttacks;
+                StartCoroutine(Attack());
+            }
+        }
+
+    }
+
+    IEnumerator Attack()
+    {
+        pathfinder.enabled = false;
+        currentState = State.Attacking;
+
+        Vector3 originalPosition = transform.position;
+        Vector3 dirToTarget = (target.position - transform.position).normalized;
+        Vector3 attackPosition = target.position - dirToTarget * (myCollisionRadius);
+
+        float attackSpeed = 2f;
+
+        skinMaterial.color = Color.red;
+
+        float percent = 0;
+        while (percent <= 1)
+        {
+            percent += Time.deltaTime * attackSpeed;
+            float interpolation = (-Mathf.Pow(percent, 2) + percent) * 4;
+            transform.position = Vector3.Lerp(originalPosition, attackPosition, interpolation);
+
+
+            yield return null;
+        }
+
+        skinMaterial.color = originalColor;
+
+        currentState = State.Chasing;
+        pathfinder.enabled = true;
     }
 
     IEnumerator UpdatePath()
@@ -26,10 +89,14 @@ public class Enemy : LivingEntity
 
         while (target != null)
         {
-            Vector3 targetPostion = new Vector3(target.position.x, 0, target.position.z);
-            if (!dead)
+            if (currentState == State.Chasing)
             {
-                pathfinder.SetDestination(targetPostion);
+                Vector3 dirToTarget = (target.position - transform.position).normalized;
+                Vector3 targetPostion = target.position - dirToTarget * (myCollisionRadius + targetCollisionRadius + attackDistanceThreshold / 2);
+                if (!dead)
+                {
+                    pathfinder.SetDestination(targetPostion);
+                }
             }
             yield return new WaitForSeconds(refreshrate);
         }
