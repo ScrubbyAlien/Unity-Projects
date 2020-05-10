f52dacc8fda1336e9e7de20fd22cd152792a0223
Samuel Eskilson <samuel.eskilson@gmail.com>
2020-05-10 22:05:01 +0200

    fysiksimluationsprojekt skapat och enemy spawning fixat
    enemies now spawn on random tiles

---
+++ Fysik Simulation/Assets/Launch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Launch : MonoBehaviour
+{
+    public float intialXVelocity;
+    public GameObject inputField;
+
+    float intialYVelocity;
+    Vector2 intialVelocity;
+    Rigidbody2D mRigidbody;
+
+    void Start()
+    {
+        mRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            intialYVelocity = float.Parse(inputField.GetComponent<Text>().text);
+            intialVelocity = new Vector2(intialXVelocity, intialYVelocity);
+
+            mRigidbody.AddForce(intialVelocity, ForceMode2D.Impulse);
+
+        }
+    }
+}
