180245db06a5586bcf28f9e8480c0ace1d1c6e6c
Samuel Eskilson <samuel.eskilson@gmail.com>
2020-06-17 15:32:07 +0200

    made walls retract into ground

---
+++ OneRoomGame/Assets/Scripts/Deobstructor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deobstructor : MonoBehaviour
+{
+    public Transform ObstructingWalls;
+
+    public float downScale;
+    Vector3 originalScale;
+
+    bool wallsDown;
+
+    void Start()
+    {
+        wallsDown = false;
+        originalScale = ObstructingWalls.localScale;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("o"))
+        {
+            if (wallsDown)
+            {
+                ObstructingWalls.localScale = originalScale;
+                wallsDown = false;
+            }
+            else
+            {
+                ObstructingWalls.localScale = new Vector3(originalScale.x, downScale, originalScale.z);
+                wallsDown = true;
+            }
+        }
+    }
+}
---
+++ OneRoomGame/Assets/Scripts/LightImprove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class LightImprove : MonoBehaviour
+{
+    Light thisLight;
+    public float bias;
+
+    void Start()
+    {
+        thisLight = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        thisLight.shadowBias = bias;
+    }
+}
