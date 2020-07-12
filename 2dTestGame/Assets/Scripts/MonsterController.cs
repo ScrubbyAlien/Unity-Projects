c972c70442e2b4778b04e9884b34e82bd63190dd
Samuel Eskilson <samuel.eskilson@gmail.com>
2020-07-12 23:42:52 +0200

    Started project
    fixed with adding tilemaps, pixel perfect camera, sprites and some animation

---
+++ 2dTestGame/Assets/Scripts/MonsterController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterController : MonoBehaviour
+{
+
+    private int randX;
+    private int randY;
+    private float waitTime;
+
+    void Start()
+    {
+        StartCoroutine(RandomWalking());
+    }
+
+    IEnumerator RandomWalking()
+    {
+        while (true)
+        {
+            randX = (int)Random.Range(-5, 5);
+            randY = (int)Random.Range(-5, 5);
+            waitTime = Random.Range(2, 5);
+
+            if (transform.position.y + randY > 5 || transform.position.y + randY < -5)
+            {
+                randY = 0;
+            }
+
+            if (transform.position.x + randX > 8 || transform.position.x + randX < -8)
+            {
+                randX = 0;
+            }
+
+            transform.Translate(new Vector2(randX, randY), Space.World);
+            yield return new WaitForSecondsRealtime(waitTime);
+        }
+    }
+}
